0f8535d8c6a9104283a3ae664c5e38634cf5664e
gituser <quanttrader500@gmail.com>
2019-01-05 11:43:32 +0100

    added babel

---
+++ src/CashRegisterTest.js
@@ -0,0 +1,4 @@
+import CashRegister from './CashRegister';
+
+const cashRegister = new CashRegister();
+cashRegister.makeChange(400, 555);    // [100, 50, 5]
---
+++ src/declarative-programming.js
@@ -0,0 +1,6 @@
+let arr = [1,2,3,4,5];
+let arr2 = [];
+
+arr2 = arr.map( n => n*2 );
+
+console.log(arr2);
