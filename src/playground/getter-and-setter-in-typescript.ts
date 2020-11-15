f48842764eabcb3347a0e0ac3616a68abc7dbc4a
quant500 <quanttrader500@gmail.com>
2020-11-15 15:23:18 +0100

    add example for getter and setter

---
+++ src/playground/getter-and-setter-in-typescript.ts
@@ -0,0 +1,33 @@
+class Foo {
+    private _booleanProperty: boolean = false;
+    private _stringProperty: string = "";
+
+    get stringProperty(): string {
+        return this._stringProperty;
+    }
+
+    set stringProperty(value: string) {
+        this._stringProperty = value;
+    }
+
+    get booleanProperty() {
+        return this._booleanProperty;
+    }
+
+    set booleanProperty(newBar) {
+        this._booleanProperty = newBar;
+    }
+}
+
+const foo = new Foo();
+
+// tslint:disable-next-line:no-console
+console.log(foo.booleanProperty);
+console.log(foo.stringProperty);
+
+foo.booleanProperty = true;
+foo.stringProperty = "new string value";
+
+// tslint:disable-next-line:no-console
+console.log(foo.booleanProperty);
+console.log(foo.stringProperty);
