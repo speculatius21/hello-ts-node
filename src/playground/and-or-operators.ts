83b1ba3ef674caa129ffb7979b17e3654ce90929
quant500 <quanttrader500@gmail.com>
2020-11-15 14:32:38 +0100

    added example for index signature

--- src/playground/and-or-operators.ts
+++ src/playground/and-or-operators.ts
@@ -1,5 +1,5 @@
-//let sz = 'string value';
-//sz = null;
+// let sz = 'string value';
+// sz = null;
 let sz = null;
 
 console.log(sz || 'default');
--- src/playground/function-types.ts
+++ src/playground/function-types.ts
@@ -3,8 +3,14 @@
 // }
 
 // tslint:disable-next-line:only-arrow-functions
-let add = (a: number, b: number) => a + b;
+// let add = (a: number, b: number) => { return a + b; };
+
+// tslint:disable-next-line:only-arrow-functions
+let add = function(a: number, b: number) { return a + b; };
+
 // let add = new Function('a', 'b', 'return a+b');
 
+// tslint:disable-next-line:no-console
 console.log(add(4, 5)); // returns: 9
-console.log(add);       // returns: [Function: anonymous]
+// tslint:disable-next-line:no-console
+console.log(add);       // returns: [Function: add]
