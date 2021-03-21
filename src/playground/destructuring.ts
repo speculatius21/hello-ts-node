549e74a4ee621c8a726ec432bb86ae45ae0fc67d
quant500 <quanttrader500@gmail.com>
2021-03-21 09:11:46 +0100

    add new examples

---
+++ src/playground/destructuring.ts
@@ -0,0 +1,20 @@
+// Destructuring an object
+const numbers = {
+    one: 1,
+    two: 2
+};
+
+const {one, two} = numbers;
+
+console.log(one); // 1
+console.log(two); // 2
+
+
+
+// Destructuring an array
+const numbers2 = [1, 2, 3, 4, 5];
+
+const [one2, two2] = numbers2;
+
+console.log(one); // 1
+console.log(two); // 2
