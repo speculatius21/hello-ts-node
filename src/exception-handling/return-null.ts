6af030f7bd2e5cea09d6bfe3572fe6079587b87e
speculatius21 <quanttrader500@gmail.com>
2021-03-28 20:15:06 +0200

    add example code for exception handling

---
+++ src/exception-handling/return-null.ts
@@ -0,0 +1,26 @@
+function ask(): string | null {
+    // return prompt('When is your birthday?');
+    return '07.10.1971';
+}
+
+function parse(birthday: string | null): Date | null {
+    // tslint:disable-next-line:no-shadowed-variable
+    const date = new Date(birthday || '');
+    if(!isValid(date)) {
+        return null;
+    }
+    return date;
+}
+
+// tslint:disable-next-line:no-shadowed-variable
+function isValid(date: Date) {
+    return Object.prototype.toString.call(date) === '[object Date]'
+        && !isNaN(date.getTime());
+}
+
+let date = parse(ask());
+if (date) {
+    console.info('Date is', date.toISOString());
+} else {
+    console.error('Error parsing date for some reason');
+}
