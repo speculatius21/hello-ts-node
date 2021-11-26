137fa7fb96fe032f0a9097e5a2cb1bb86aa598f6
speculatius21 <quanttrader500@gmail.com>
2021-11-26 20:32:33 +0100

    beatufied file

--- src/observable-examples/merge.ts
+++ src/observable-examples/merge.ts
@@ -2,13 +2,18 @@ import {interval, merge} from "rxjs";
 import {mapTo} from "rxjs/operators";
 
 // emits every second
-const ones = interval(1000);
+const ones$ = interval(1000);
 // emit every 2 seconds
-const twos = interval(2000);
+const twos$ = interval(2000);
 
 merge( // <-------- the merge-operator joins two or more observables into one observable
-    ones.pipe(mapTo("Ones")), // <---- pipe() is used to attach operators to observables
-    twos.pipe(mapTo("Twos"))
+    ones$.pipe(
+        mapTo("Ones")
+    ), // <---- pipe() is used to attach operators to observables
+    twos$.pipe(
+        mapTo("Twos")
+    )
+// tslint:disable-next-line:no-console
 ).subscribe(val => console.log(val));
 
 // Output : "Ones", "Twos", "Ones, "Ones", "Twos", "Ones", "Ones"...
